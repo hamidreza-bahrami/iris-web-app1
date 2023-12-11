d2e4a9fb4ba93831b17f03e5f3738ab6c20a2b9f
hamidreza bahrami <hamidr.bahraami@gmail.com>
2023-12-11 17:39:27 +0330

    initial commit

---
+++ iris-app.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+
+st.write("""
+# Iris Flower Prediction
+""")
+
+st.sidebar.header('Enter your parameters')
+
+def user_input_features():
+    sepal_length = st.sidebar.slider('Sepal Length', 4.3, 7.9, 5.4)
+    sepal_width = st.sidebar.slider('Sepal Width', 2.0, 4.4, 3.4)
+    petal_length = st.sidebar.slider('Petal Length', 1.0, 6.9, 1.3)
+    petal_width = st.sidebar.slider('Petal Width', 0.1, 2.5, 0.2)
+    data = {'sepal_length': sepal_length,
+            'sepal_width': sepal_width,
+            'petal_length': petal_length,
+            'petal_width': petal_width}
+    features = pd.DataFrame(data, index = [0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input Parameters')
+st.write(df)
+
+iris = datasets.load_iris()
+x = iris.data
+y = iris.target
+
+model = RandomForestClassifier()
+model.fit(x, y)
+
+prediction = model.predict(df)
+prediction_proba = model.predict_proba(df)
+
+st.subheader('Class lables and their index number')
+st.write(iris.target_names)
+
+st.subheader('Prediction')
+st.write(iris.target_names[prediction])
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
+
+
