328ac1e2d6f55fd8cb792f31431b8c966f160f7f
hamidreza-bahrami <150763463+hamidreza-bahrami@users.noreply.github.com>
2024-05-04 04:57:11 -0700

    Update iris-app.py

--- iris-app.py
+++ iris-app.py
@@ -2,46 +2,54 @@ import streamlit as st
 import pandas as pd
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
+import time
 
-st.title('مدل تشخیص گل زنبق')
-st.sidebar.header('پارامتر هارا وارد کنید')
-st.write("<h4 style='text-align: center; color: gray;'>Robo-Ai.ir طراحی شده توسط</h4>", unsafe_allow_html=True)
-st.link_button("Robo-Ai بازگشت به", "https://robo-ai.ir")
+def show_page():
+    st.write("<h1 style='text-align: center; color: blue;'>تشخیص نوع گل با هوش مصنوعی</h1>", unsafe_allow_html=True)
+    st.write("<h4 style='text-align: center; color: gray;'>Robo-Ai.ir طراحی شده توسط</h4>", unsafe_allow_html=True)
+    st.link_button("Robo-Ai بازگشت به", "https://robo-ai.ir")
 
-def user_input_features():
-    sepal_length = st.sidebar.slider('طول کاسبرگ', 4.3, 7.9, 5.4)
-    sepal_width = st.sidebar.slider('عرض کاسبرگ', 2.0, 4.4, 3.4)
-    petal_length = st.sidebar.slider('طول گلبرگ', 1.0, 6.9, 1.3)
-    petal_width = st.sidebar.slider('عرض گلبرگ', 0.1, 2.5, 0.2)
-    data = {'sepal_length': sepal_length,
+    def user_input_features():
+        sepal_length = st.slider('طول کاسبرگ', 4.3, 7.9, 5.4)
+        sepal_width = st.slider('عرض کاسبرگ', 2.0, 4.4, 3.4)
+        petal_length = st.slider('طول گلبرگ', 1.0, 6.9, 1.3)
+        petal_width = st.slider('عرض گلبرگ', 0.1, 2.5, 0.2)
+        data = {'sepal_length': sepal_length,
             'sepal_width': sepal_width,
             'petal_length': petal_length,
             'petal_width': petal_width}
-    features = pd.DataFrame(data, index = [0])
-    return features
+        features = pd.DataFrame(data, index = [0])
+        return features
 
-df = user_input_features()
+    df = user_input_features()
 
-st.subheader('پارامتر های وارد شده')
-st.write(df)
+    iris = datasets.load_iris()
+    x = iris.data
+    y = iris.target
 
-iris = datasets.load_iris()
-x = iris.data
-y = iris.target
+    model = RandomForestClassifier()
+    model.fit(x, y)
 
-model = RandomForestClassifier()
-model.fit(x, y)
+    ibutton = st.button('شناسایی')
+    if ibutton:
+        with st.chat_message("assistant"):
+            with st.spinner('''درحال شناسایی'''):
+                time.sleep(2)
+                st.success(u'\u2713''شناسایی انجام شد')
+        prediction = model.predict(df)
+        prediction_proba = model.predict_proba(df)
+        if prediction == 0:
+            st.write("<h4 style='text-align: right; color: gray;'>بر اساس تحلیل من نوع گل زنبق وارد شده ، ستوسا است</h4>", unsafe_allow_html=True)
+            st.write("<h4 style='text-align: left; color: gray;'>Based on my analysis, this dimension belong to Setosa</h4>", unsafe_allow_html=True)
+            st.image('setosa.jpg')
 
-prediction = model.predict(df)
-prediction_proba = model.predict_proba(df)
+        elif prediction == 1:
+            st.write("<h4 style='text-align: right; color: gray;'>بر اساس تحلیل من نوع گل زنبق وارد شده ، نوع گل ورسیکالر است</h4>", unsafe_allow_html=True)
+            st.write("<h4 style='text-align: left; color: gray;'>Based on my analysis, this dimension belong to Versicolor</h4>", unsafe_allow_html=True)
+            st.image('versicolor.jpeg')
 
-st.subheader('نوع گل قابل تشخیص')
-st.write(iris.target_names)
-
-st.subheader('تشخیص نهایی')
-st.write(iris.target_names[prediction])
-
-st.subheader('احتمال پیش بینی هر کدام بر اساس این پارامترها')
-st.write(prediction_proba)
-
-
+        else:
+            st.write("<h4 style='text-align: right; color: gray;'>بر اساس تحلیل من نوع گل زنبق وارد شده ، نوع گل ویرجینیکا است</h4>", unsafe_allow_html=True)
+            st.write("<h4 style='text-align: left; color: gray;'>Based on my analysis, this dimension belong to Virginica</h4>", unsafe_allow_html=True)
+            st.image('virginica.jpg')
+show_page()
